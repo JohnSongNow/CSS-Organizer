9fd243051a45e7c3fdcb0e4a359b7dfdb01280a6
John Song <John.Song.Toronto@gmail.com>
2015-10-22 23:02:46 -0400

    Fixed a block typo.
    Fixed a block typo in page.py.

--- src/Python/block.py
+++ src/Python/block.py
@@ -5,33 +5,33 @@ class Block():
     the block does not check if the props within the CSS is valid and must be explicatly
     called.
     """
-    
+
     def __init__(self, name, props=[]):
         self._props = props
         self._name = name
-    
-    
+
+
     def changeName(self, name):
         self._name = name
-    
-    
+
+
     def changeProps(self, props):
         self._props = props
-        
-        
+
+
     def removePropByName(self, name):
         '''
         Removes the property within the list
         '''
         # Gets ths removed propr, None if not present
         removed_prop = self.containsProperty(name)
-        
+
         # Removing the prop
         if(removed_prop == None):
             return
         self._props.remove(removed_prop)
-            
-        
+
+
     def containsProperty(self, name):
         """
         Checks if the block contains the property with
@@ -39,14 +39,14 @@ class Block():
         """
         # New list of only the property with the wanted name
         new_list = [x for x in self._props if x.getName() == name]
-        
+
         # If nothing fits the description
         if(len(new_list) == 0):
             return None
         # Returns the first element
         return new_list[0]
-    
-    
+
+
     def addProp(self, prop):
         '''
         Adds a prop to list, if the prop exists replace
@@ -61,7 +61,7 @@ class Block():
             # Swaps the values
             current_prop.setValue(prop.getValue())
 
-    
+
     def __str__(self):
         """
         Returns the block as a string
