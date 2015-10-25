436315e01b66a7a500cfd8392cf615d38ca42907
John Song <John.Song.Toronto@gmail.com>
2015-10-24 23:57:05 -0400

    Added some test files and started parsing.

--- src/Python/css-organizer.py
+++ src/Python/css-organizer.py
@@ -4,9 +4,39 @@ from page import *
 
 OPTIONS = dict()
 
+FILE_LIMIT = 25
+FILE_SIZE_LIMIT = 5000
+FILES = []
 
-def importCssFile():
+def importCSSFiles(file_names, file_path=''):
+    '''
+    Imports the CSS files in a directory into
+    the files list, note that the files are
+    cleared before imported.
+    '''
+    FILES = []
+
+    # Reading and storing the lines in our array
+    for file_name in file_names:
+        with open(file_path + file_name + '.css') as f:
+            FILES.append(f.read().splitlines())
+            f.close()
+
+    print(FILES[0])
     return
+
+def text_to_page(lines):
+    '''
+    Returns the
+    '''
+    return
+
+def addCSSFile(file_names, file_part=''):
+    '''
+    Adds a CSS file into the existing files list.
+    '''
+    return
+
 
 def loadOptions():
     """
@@ -27,3 +57,5 @@ test_block.addProp(test_prop2)
 
 # Printing out the block
 print(test_block)
+
+importCSSFiles(['expected-test', 'initial-test'], 'test/')
